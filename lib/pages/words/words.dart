5e2ca2738dac882764e7a0694d050504e34ece94
voledemar <voledemar@inbox.ru>
2020-08-22 22:40:17 +0300

    #7 refactor words page UI

--- lib/pages/words/words.dart
+++ lib/pages/words/words.dart
@@ -9,20 +9,40 @@ import 'package:laera/models/word.dart';
 import 'package:laera/pages/words/widgets/word_card.dart';
 import 'package:laera/repos/word.dart';
 
-class WordsPage extends StatelessWidget {
+class WordsPage extends StatefulWidget {
   final WordRepo _wordRepo;
 
   const WordsPage(this._wordRepo);
 
   @override
+  _WordsPageState createState() => _WordsPageState();
+}
+
+class _WordsPageState extends State<WordsPage> {
+  @override
   Widget build(BuildContext context) => asyncBuild(
-        future: _wordRepo.getAll(),
+        future: widget._wordRepo.getAll(),
         builder: (data) {
           var widgets = <Widget>[];
           (data as List<Word> ?? []).forEach((word) {
-            widgets.add(WordCard(word, _wordRepo.delete));
+            widgets.add(
+              WordCard(word, widget._wordRepo.delete, () => setState(() {})),
+            );
+            widgets.add(Divider());
           });
-          return ListView(children: widgets);
+          return widgets.length > 0
+              ? Center(
+                  child: FractionallySizedBox(
+                    widthFactor: 0.9,
+                    heightFactor: 0.95,
+                    child: ListView(children: widgets),
+                  ),
+                )
+              : Center(
+                  child: Text(
+                  "No words. Add some",
+                  textScaleFactor: 1.5,
+                ));
         },
       );
 }
