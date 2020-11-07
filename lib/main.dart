926cb461db104dd755e65c6d5f3052d59833c0fd
voledemar <voledemar@inbox.ru>
2020-11-07 14:49:57 +0300

    #22 create navbar widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,41 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-import 'package:laera/app.dart';
+import 'package:laera/pages/flow/flow.dart';
+import 'package:laera/pages/new/new.dart';
+import 'package:laera/pages/words/words.dart';
+import 'package:laera/repos/sources/db.dart';
+import 'package:laera/repos/word.dart';
+import 'package:laera/widgets/navbar.dart';
 
-void main() => runApp(
-      MaterialApp(
-        theme: ThemeData(
-          primarySwatch: Colors.lightGreen,
-        ),
-        home: AppWidget(),
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+      ),
+      home: Navbar(
+        elements: [
+          NavbarElement(
+            label: 'Flow',
+            icon: Icons.all_inclusive,
+            page: FlowPage(WordRepo(DB.db)),
+          ),
+          NavbarElement(
+            label: 'New',
+            icon: Icons.add,
+            page: NewPage(WordRepo(DB.db)),
+          ),
+          NavbarElement(
+            label: 'Words',
+            icon: Icons.list,
+            page: WordsPage(WordRepo(DB.db)),
+          ),
+        ],
       ),
     );
+  }
+}
