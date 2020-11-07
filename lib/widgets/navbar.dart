fa29f1f33d571e8d91f36be56adbd632ac69a651
voledemar <voledemar@inbox.ru>
2020-11-08 01:36:02 +0300

    #22 fix possible null dereference

--- lib/widgets/navbar.dart
+++ lib/widgets/navbar.dart
@@ -12,8 +12,8 @@ class Navbar extends StatefulWidget {
   Navbar({
     @required List<NavbarElement> elements,
     this.defaultElementIndex = 0,
-  })  : pages = [for (final element in elements) element.page],
-        items = [for (final element in elements) element.item];
+  })  : pages = [for (final element in elements ?? []) element.page],
+        items = [for (final element in elements ?? []) element.item];
 
   Widget pageAt(int index) =>
       index < pages.length ? pages[index] : pages[defaultElementIndex];
