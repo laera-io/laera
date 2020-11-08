87d67ecc1f21016eef8ba1cc8d471351cd087ec1
voledemar <voledemar@inbox.ru>
2020-11-08 17:20:15 +0300

    #22 refactor words page

--- lib/pages/words/words.dart
+++ lib/pages/words/words.dart
@@ -3,7 +3,6 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-
 import 'package:laera/widgets/async.dart';
 import 'package:laera/models/word.dart';
 import 'package:laera/repos/word.dart';
@@ -18,44 +17,51 @@ class WordsPage extends StatefulWidget {
 }
 
 class _WordsPageState extends State<WordsPage> {
+  static const textScaleFactor = 1.3;
+  static const leadingSpaceScaleFactor = 0.05;
+
   @override
   Widget build(BuildContext context) {
     return Async(
       future: widget._wordRepo.getAll(),
       builder: (data) {
+        final words = data as List<Word> ?? [];
+        if (words.isEmpty) {
+          return const Center(
+            child: Text(
+              "No words. Add some",
+              textScaleFactor: 1.5,
+            ),
+          );
+        }
         var widgets = <Widget>[
-          Container(height: MediaQuery.of(context).size.width * 0.05)
+          Container(
+            height: MediaQuery.of(context).size.width * leadingSpaceScaleFactor,
+          ),
         ];
-        (data as List<Word> ?? []).forEach((word) {
+        (words).forEach((word) {
           widgets.add(
             ListTile(
               title: Text(
                 word.word,
-                textScaleFactor: 1.3,
+                textScaleFactor: textScaleFactor,
               ),
               subtitle: Text(word.translation),
               trailing: IconButton(
-                icon: Icon(Icons.delete),
+                icon: const Icon(Icons.delete),
                 color: Colors.red,
-                onPressed: () {
-                  widget._wordRepo.delete(word.id);
-                  setState(() {});
-                },
+                onPressed: () => setState(
+                  () => widget._wordRepo.delete(word.id),
+                ),
               ),
             ),
           );
         });
-        return widgets.length > 0
-            ? Center(
-                child: ListView(
-                  children: widgets,
-                ),
-              )
-            : Center(
-                child: Text(
-                "No words. Add some",
-                textScaleFactor: 1.5,
-              ));
+        return Center(
+          child: ListView(
+            children: widgets,
+          ),
+        );
       },
     );
   }
