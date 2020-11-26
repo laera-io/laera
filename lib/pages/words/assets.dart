d34f20c7ccbd889f52751be3e148fd38362f2664
Vladimir Poliakov <voledemar@inbox.ru>
2020-11-26 23:19:16 +0300

    #43 Split words page into tabs (#44)
    * #43 add Tabbar widget
    
    * #43 move Words page to My Words tab
    
    * #43 create empty Assets tab
    
    * #43 use Tabbar in Words page

---
+++ lib/pages/words/assets.dart
@@ -0,0 +1,20 @@
+// Copyright 2020 The Laera Authors. All rights reserved.
+// Use of this source code is governed by a MIT license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:laera/repos/word.dart';
+
+class AssestsPage extends StatelessWidget {
+  const AssestsPage(this._wordRepo);
+
+  final WordRepo _wordRepo;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement
+    return Center(
+      child: Text("under development 👷", textScaleFactor: 1.5),
+    );
+  }
+}
